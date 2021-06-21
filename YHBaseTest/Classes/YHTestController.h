0f4ed8fce7761e640c5c5ac97584ed51c69f13f4
小河 <maoyunhe@zhaojiafang.com>
2021-06-21 16:33:45 +0800

    添加测试代码

---
+++ YHBaseTest/Classes/YHTestController.h
@@ -0,0 +1,19 @@
+//
+//  YHTestController.h
+//  YHBaseTest_Example
+//
+//  Created by 小河 on 2021/6/21.
+//  Copyright © 2021 AugustCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YHTestController : UIViewController
+
+- (void)testPrint;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YHBaseTest/Classes/YHTestController.m
@@ -0,0 +1,26 @@
+//
+//  YHTestController.m
+//  YHBaseTest_Example
+//
+//  Created by 小河 on 2021/6/21.
+//  Copyright © 2021 AugustCloud. All rights reserved.
+//
+
+#import "YHTestController.h"
+
+@interface YHTestController ()
+
+@end
+
+@implementation YHTestController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)testPrint {
+    NSLog(@"啊哈哈，我的pod的打印");
+}
+
+@end
